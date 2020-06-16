bfde92594c8282ed2b520294ed3ba2a7a6010d26
Alexis Gomes <alexis.gomes19@gmail.com>
2020-06-16 18:03:29 +0200

    add comments and rm prints

--- main.py
+++ main.py
@@ -6,21 +6,17 @@ from JsonEncoder import json_encode, json_decode
 from MyClass import MyClass
 
 # Encode a dict
-my_dict = {
+json_encode({
     "name": "Paul",
     "birthdate": arrow.get("1992-02-22").datetime,
     "decimal_number": Decimal(14.456),
     "float_number": 14.456,
     "int_number": 122
-}
-
-json_encode(my_dict)
-# '{"name": "Paul", "birthdate": "1992-02-22T00:00:00+00:00", "float_number": 14.456, "int_number": 122}'
+})
 
 # Encode a custom object
 my_obj = MyClass("value1", "value2")
 json_encode(my_obj)
-# "{\"value1\": \"value1\", \"value2\": \"value2\"}"
 
 # Encode nested dicts
 json_encode({
@@ -49,11 +45,11 @@ json_encode({
     ]
 })
 
-# Decode a string
+# Decode a dict with various types
 json_decode('{"name": "Paul", "birthdate": "1992-02-22T00:00:00+00:00", "float_number": 14.456, "int_number": 122}')
 
-
+# Decode a dict, with nested dicts and list
 json_decode('{"name": "Paul", "birthdate": "1992-02-22T00:00:00+00:00", "decimal_number": 14.46, "float_number": 14.456, "int_number": 122, "my_dict": {"value_1": 1, "value_2": "2"}, "list": [{"value_1": 1, "value_2": "2"}, {"value_1": 1, "value_2": "2"}, {"value_1": 1, "value_2": "2"}]}')
 
-res = json_decode("{\"value1\": \"value1\", \"value2\": \"value2\"}")
-pprint(res)
+# Decode an object of MyClass
+json_decode("{\"value1\": \"value1\", \"value2\": \"value2\"}")
